26805077f4855eef6b6e380ac8328da9228d27d6
cshs20120831 <cshs20120831@gmail.com>
2017-03-16 00:52:14 +0800

    first experiment

---
+++ sketch_mar1.2.ino
@@ -0,0 +1,30 @@
+const byte LED=13;
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.print("Hello ");
+  Serial.println("World.");
+  pinMode(LED,OUTPUT);
+  
+}
+void loop() 
+{
+  char val;
+  if(Serial.available() )
+ {
+  val = Serial.read();
+  switch(val)
+  {
+    case '0':
+      digitalWrite(LED,LOW);
+      Serial.println("LED OFF");
+      break;
+    case '1':
+      digitalWrite(LED,HIGH);
+      Serial.println("LED ON");
+      break;
+      
+  }
+ } 
+
+}
