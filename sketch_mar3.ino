242515ed63786a1c87154edefd7eca84202d4d1d
cshs20120831 <cshs20120831@gmail.com>
2017-03-16 00:54:54 +0800

    third experiment

---
+++ sketch_mar3.ino
@@ -0,0 +1,27 @@
+float R1=1.91;
+float R2=965500;
+float sensorValue,sensorVoltage;
+float  ResisterValue;
+void setup() 
+{
+  pinMode(A1,INPUT);
+  ResisterValue = 965501.91;
+  Serial.begin(9600);
+
+}
+
+void loop() 
+{
+  int i;
+  for(i=0;i<=5;i++)
+    {
+      sensorValue=0.85*sensorValue+0.15*analogRead(A1);
+     }
+   sensorVoltage=sensorValue*5/1024;
+   R1=sensorVoltage*965501.91/5;
+   Serial.println(sensorValue);
+   Serial.println(sensorVoltage);
+   Serial.println(R1);
+   delay(1000);
+   
+}
