2272667cbe16019d4fafb7a8cdbc2d00686f5cba
cshs20120831 <cshs20120831@gmail.com>
2017-03-16 00:56:05 +0800

    fourth experimet

---
+++ _4.ino
@@ -0,0 +1,21 @@
+float sensorValue,sensorVoltage;
+void setup() 
+{
+  pinMode(A1,INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() 
+{
+  int i;
+  for(i=0;i<=5;i++)
+    {
+      sensorValue=0.7*sensorValue+0.3*analogRead(A1);
+     }
+   sensorVoltage=sensorValue*5/1024;
+  
+   Serial.println(sensorVoltage);
+   delay(1000);
+   
+}
